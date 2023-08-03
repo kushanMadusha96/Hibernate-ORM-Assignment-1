d44c9f9833674e207ec5d4ed525d061b03fd7371
kushanMadusha96 <kushanmadusha96@gmail.com>
2023-08-03 10:22:25 +0530

    done part 1

---
+++ part1/src/main/java/entity/Author.java
@@ -0,0 +1,15 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String authorId;
+    private String authorName;
+    private String country;
+}
